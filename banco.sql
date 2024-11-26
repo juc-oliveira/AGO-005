514eeef0a06398d6c30fe4138d96affe43f681fc
PimentaGit <160744002+PimentaGit@users.noreply.github.com>
2024-11-26 19:24:15 -0300

    AB#44

--- banco.sql
+++ banco.sql
@@ -27,3 +27,22 @@ observacao VARCHAR(150),
 FK_cliente_codigo INT NOT NULL,
 FOREIGN KEY (FK_cliente_codigo) REFERENCES Cliente (PK_cliente_codigo)
 );
+
+- Pet Adoção
+
+CREATE TABLE PetAdocao (
+PK_petadocao_codigo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(150) NOT NULL,
+idade INT NOT NULL,
+sexo VARCHAR(45) NOT NULL,
+pelo VARCHAR(45),
+cor_pelo VARCHAR(45),
+temperamento VARCHAR(45),
+situacao VARCHAR(150),
+porte VARCHAR(45),
+raca VARCHAR(45),
+tipo VARCHAR(45),
+observacao VARCHAR(150),
+imagem VARCHAR(100),
+FK_codigo_cliente INT,
+FOREIGN KEY (FK_codigo_cliente) REFERENCES Cliente(PK_cliente_codigo));
